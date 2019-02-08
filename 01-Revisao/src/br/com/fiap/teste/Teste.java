a5e2175e4cae08e6e6b294669a8f4fe40c4e8ee9
Logon PF Fiap <logonpflocal@fiap.com.br>
2019-02-08 09:38:14 -0200

    Criação de anotações java

--- 01-Revisao/src/br/com/fiap/teste/Teste.java
+++ 01-Revisao/src/br/com/fiap/teste/Teste.java
@@ -2,6 +2,7 @@ package br.com.fiap.teste;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 import br.com.fiap.bean.Carro;
@@ -9,6 +10,7 @@ import br.com.fiap.bean.Transmissao;
 
 public class Teste {
 
+	@SuppressWarnings("all")
 	public static void main(String args[]) {
 		Carro carro = new Carro();
 		carro.setCambio(Transmissao.SEMI_AUTOMATICO);
@@ -23,6 +25,8 @@ public class Teste {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy, hh:mm");
 		System.out.println(sdf.format(data.getTime()));
 		System.out.println(sdf.format(data2.getTime()));
+		
+		new Date("");
 		
 	}
 	
